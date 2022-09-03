2896760e81d25b60cb70d259a151d33da0c64c14
Thiago Silva <thiago380tt@gmail.com>
2022-09-02 22:56:04 -0300

    Form de autenticação

--- src/components/Sidebar/SidebarNav.tsx
+++ src/components/Sidebar/SidebarNav.tsx
@@ -8,12 +8,12 @@ export function SidebarNav() {
         <Stack spacing="12" align="fle">
         <NavSection title="GERAL">
             <NavLink icon={RiDashboardLine} href="/dashboard">Dashboard</NavLink>
-            <NavLink icon={RiContactsLine} href="users" >Usuários</NavLink>
+            <NavLink icon={RiContactsLine} href="/users" >Usuários</NavLink>
         </NavSection>
 
         <NavSection title="AUTOMAÇÂO">
             <NavLink icon={RiInputMethodLine} href="/forms">Formulário</NavLink>
-            <NavLink icon={RiGitMergeLine} href="automation">Automação</NavLink>
+            <NavLink icon={RiGitMergeLine} href="/automation">Automação</NavLink>
         </NavSection>
       </Stack>
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,22 @@
 import { Button, Flex, Stack } from '@chakra-ui/react'
+import { SubmitHandler, useForm } from "react-hook-form"
 import { Input } from '../components/Form/Input'
 
 
+type SignInFormData = {
+  email: string;
+  passoword: string;
+}
+
 export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm()
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 200));
+
+    console.log(values);
+  }
+
   return (
     <Flex
       w="100vw"
@@ -18,13 +32,16 @@ export default function SignIn() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
         >
         <Stack spacing="4">
-          <Input name='email' type="email" label='E-mail' />
-          <Input name='password' type="password" label='Senha' />
+          <Input name='email' type="email" label='E-mail' {...register('email')} />
+          <Input name='password' type="password" label='Senha' {...register('password')} />
         </Stack>
 
-        <Button type='submit' mt="6" colorScheme="pink" size="lg" > Entrada</Button>
+        <Button type='submit' mt="6" colorScheme="pink" size="lg" isLoading={formState.isSubmitting}>
+           Entrada
+        </Button>
 
       </Flex>
     
